b3f6eee36ad63c03e87eaa9d58de9d851c1fbc77
AnnJakubel <annabel.jakubel@gmail.com>
2023-06-03 09:36:42 +0300

    Added classes Branch, JobTitleHistory and JobTitleList. Added variables to classes.

--- DatabaseTask/DatabaseTask.Core/Domain/Branch.cs
+++ DatabaseTask/DatabaseTask.Core/Domain/Branch.cs
@@ -2,5 +2,13 @@
 {
     public class Branch
     {
+        public Guid BranchId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+       public int RegisterCode { get; set; }
+
     }
 }
--- DatabaseTask/DatabaseTask.Core/Domain/JobTitleHistory.cs
+++ DatabaseTask/DatabaseTask.Core/Domain/JobTitleHistory.cs
@@ -1,6 +1,19 @@
-﻿namespace DatabaseTask.Core.Domain
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DatabaseTask.Core.Domain
 {
     public class JobTitleHistory
     {
+        [Key]
+        public Guid TitleHistoryId { get; set; }
+
+        public List<string> JobTitles { get; set; }
+
+        public JobTitleList JobTitleList { get; set; }
+
+        public DateTime JobTitleStartDate { get; set; }
+
+        public DateTime JobTitleDuration { get; set;}
+
     }
 }
