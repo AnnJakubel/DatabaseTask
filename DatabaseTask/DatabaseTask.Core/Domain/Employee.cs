8a7fd628c783d30848dfb8bee3e78dcf841d9006
AnnJakubel <annabel.jakubel@gmail.com>
2023-06-03 09:13:54 +0300

    Changed Employee classes

--- DatabaseTask/DatabaseTask.Core/Domain/Employee.cs
+++ DatabaseTask/DatabaseTask.Core/Domain/Employee.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DatabaseTask.Core.Domain
 {
-    internal class Class1
-    {
+    public class Employee {
+
+        [Key]
+        public Guid EmployeeId { get; set; }
+
+        public string Name { get; set; }
+
+        public JobTitleHistory JobTitleHistory { get; set; }
+
+        public Branch Branch { get; set; }
+
+        public int SocialSecurityNumber { get; set; }
+
+        public string Address { get; set; }
+
+        public string BankAccount { get; set; }
+
+        public DateTime ContractBeginningDate { get; set; }
+
+        public DateTime? ContractEndingDate { get; set; }
+
+
+
     }
 }
