f4a18fa4780032ed3d24f7825ab2e7af424ace3f
AnnJakubel <annabel.jakubel@gmail.com>
2023-06-12 11:16:07 +0300

    Added missing classes to DbContext, also renamed EmployeeChildrenConnection

--- DatabaseTask/DatabaseTask.Core/Domain/Hint.cs
+++ DatabaseTask/DatabaseTask.Core/Domain/Hint.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace DatabaseTask.Core.Domain
 {
-    internal class Hint
+    public class Hint
     {
         [Key]
         public Guid HintId { get; set; }
--- DatabaseTask/DatabaseTask.Data/DatabaseTaskDbContext.cs
+++ DatabaseTask/DatabaseTask.Data/DatabaseTaskDbContext.cs
@@ -11,6 +11,19 @@ namespace DatabaseTask.Data
 
         // näide, kuidas teha, kui lisate domaini alla ühe objekti
         // migratsioonid peavad tulema siia libary-sse e TARge20.Data alla.
-        public DbSet<ExampleEmployee> Employee { get; set; }
+        public DbSet<Branch> Branches { get; set; }
+        public DbSet<Child> Children { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeChildrenConnection> EmployeeChildrenConnections { get; set; }
+        public DbSet<Hint> Hints { get; set; }
+        public DbSet<JobTitleHistory> JobTitleHistories { get; set; }
+        public DbSet<JobTitleList> JobTitleList { get; set; }
+        public DbSet<Permissions> Permissions { get; set; }
+        public DbSet<PhysicalEvaluation> PhysicalEvaluations { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+        public DbSet<RentalItem> RentalItems { get; set; }
+        public DbSet<Request> Requests { get; set; }
+        public DbSet<SickLeave> SickLeaves { get; set; }
+        public DbSet<Vacation> Vacations { get; set; }
     }
 }
