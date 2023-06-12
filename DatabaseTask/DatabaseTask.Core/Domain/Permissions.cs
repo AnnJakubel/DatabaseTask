d950b7ad4404ab64ab22765889e1f0b9e02c7c08
AnnJakubel <annabel.jakubel@gmail.com>
2023-06-12 10:41:43 +0300

    Created missing classes

---
+++ DatabaseTask/DatabaseTask.Core/Domain/Permissions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DatabaseTask.Core.Domain
+{
+    public class Permissions
+    {
+
+        [Key]
+        public Guid PermissionsId { get; set }
+
+        public JobTitleList JobTitleList { get; set; }
+
+        public Employee Employee { get; set; }
+
+        public String PermissionsDescription { get; set; }
+
+        public DateOnly PermissionsGrantedDate { get; set; }
+
+        public DateOnly PermissionsExpirationDate { get; set; }
+    }
+}
---
+++ DatabaseTask/DatabaseTask.Core/Domain/Request.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseTask.Core.Domain
+{
+    public class Request
+    {
+        [Key]
+        public Guid RequestId { get; set; }
+
+        public Employee Employee { get; set; }
+
+        public Branch Branch { get; set; }
+
+        public String RequestDescription { get; set; }
+
+        public DateOnly RequestSubmissionDate { get; set; }
+
+        public String RequestStatus { get; set; }
+        
+    }
+}
