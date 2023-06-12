cd75606e7896c0b4e94f6b9df18ab5682c912783
AnnJakubel <annabel.jakubel@gmail.com>
2023-06-12 10:32:23 +0300

    Deleted extra classes

---
+++ DatabaseTask/DatabaseTask.Core/Domain/Hint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseTask.Core.Domain
+{
+    internal class Hint
+    {
+        [Key]
+        public Guid HintId { get; set; }
+
+        public Branch Branch { get; set; }
+
+        public String HintContent { get; set; }
+
+        public DateOnly HintSubmissionDate { get; set; }
+
+        public String HintStatus { get; set; }
+
+
+    }
+}
